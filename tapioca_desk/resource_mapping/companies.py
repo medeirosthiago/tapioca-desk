057e710c3488eb357dcda8c9a1c080fdec691ba6
Thiago Medeiros de Souza <thsouzza@gmail.com>
2017-09-19 18:21:51 -0300

    Add companies on resource_mapping

---
+++ tapioca_desk/resource_mapping/companies.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+
+COMPANIES_MAPPING = {
+    'companies_list': {
+        'resource': 'companies',
+        'docs': 'http://dev.desk.com/API/companies/#list',
+        'methods': ['GET']
+    },
+    'companies_show': {
+        'resource': 'companies/{id}',
+        'docs': 'http://dev.desk.com/API/companies/#show',
+        'methods': ['GET']
+    },
+    'companies_show': {
+        'resource': 'companies/{id}',
+        'docs': 'http://dev.desk.com/API/companies/#show',
+        'methods': ['GET']
+    },
+    'companies_create': {
+        'resource': 'companies',
+        'docs': 'http://dev.desk.com/API/companies/#create',
+        'methods': ['POST']
+    },
+    'companies_update': {
+        'resource': 'companies/{id}',
+        'docs': 'http://dev.desk.com/API/companies/#update',
+        'methods': ['PATCH']
+    },
+    'companies_search': {
+        'resource': 'companies/search',
+        'docs': 'http://dev.desk.com/API/companies/#search',
+        'methods': ['GET']
+    },
+    'companies_list_cases': {
+        'resource': 'companies/{id}/cases',
+        'docs': 'http://dev.desk.com/API/companies/#cases-list',
+        'methods': ['GET']
+    },
+    'companies_customers': {
+        'resource': 'companies/{id}/customers',
+        'docs': 'http://dev.desk.com/API/companies/#customers-list',
+        'methods': ['GET']
+    },
+}
+
