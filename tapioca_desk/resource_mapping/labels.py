8df28d16925ed871b7a757ab84ed5629b88f17d9
Thiago Medeiros de Souza <thsouzza@gmail.com>
2017-09-19 18:06:50 -0300

    Add labels to resource_mapping

---
+++ tapioca_desk/resource_mapping/labels.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+
+LABELS_MAPPING = {
+    'labels_list': {
+        'resource': 'labels',
+        'docs': 'http://dev.desk.com/API/labels/#list',
+        'methods': ['GET']
+    },
+    'labels_show': {
+        'resource': 'labels/{label_id}',
+        'docs': 'http://dev.desk.com/API/labels/#show',
+        'methods': ['GET']
+    },
+    'labels_create': {
+        'resource': 'labels',
+        'docs': 'http://dev.desk.com/API/labels/#create',
+        'methods': ['POST']
+    },
+    'labels_update': {
+        'resource': 'labels/{label_id}',
+        'docs': 'http://dev.desk.com/API/labels/#update',
+        'methods': ['PATCH']
+    },
+    'labels_delete': {
+        'resource': 'labels/{label_id}',
+        'docs': 'http://dev.desk.com/API/labels/#delete',
+        'methods': ['DELETE']
+    },
+    'labels_search': {
+        'resource': 'labels/search',
+        'docs': 'http://dev.desk.com/API/labels/#search',
+        'methods': ['GET']
+    },
+}
